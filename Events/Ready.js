f5ce4072c11466f28040da931d4218d93448928d
Furkan <132396838+tyial@users.noreply.github.com>
2024-07-21 13:40:09 +0300

    Update Ready.js

--- Events/Ready.js
+++ Events/Ready.js
@@ -5,16 +5,21 @@ module.exports = client => {
     const statuses = [
         { name: `${client.user.username} hizmete hazır.`, type: "WATCHING" },
         { name: `Tyial tarafından geliştirilmiştir.`, type: "PLAYING" },
-        { name: `discord.js v14 Moderasyon Botu!`, type: "LISTENING" },
+        { name: `discord.js v14 Eğlence Botu!`, type: "LISTENING" },
     ];
 
     let currentIndex = 0;
 
     console.success(`${client.user.username} adlı hesaba başarıyla bağlanıldı.`)
+    let index = 0;
 
     setInterval(() => {
-        const status = statuses[currentIndex];
-        client.user.setPresence({ activities: [status], status: 'dnd' });
-        currentIndex = (currentIndex + 1) % statuses.length;
-    }, 5000);
+        const status = statuses[index];
+        client.user.setPresence({
+            activities: [{ name: status.name, type: status.type }],
+            status: 'online'
+        });
+
+        index = (index + 1) % statuses.length;
+    }, 10000);
 };
